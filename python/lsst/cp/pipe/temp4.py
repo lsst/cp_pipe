03f9078cc3b9c58ba213ad415ca4d0ab3652382e
Merlin Fisher-Levine <merlin.fisherlevine@gmail.com>
2018-04-05 08:06:20 -0500

    just a backup

--- python/lsst/cp/pipe/temp4.py
+++ python/lsst/cp/pipe/temp4.py
@@ -1,12 +1,7 @@
 import lsst.afw.math as afwMath
 import numpy as np
 
-def xcorrFromVisitPair(self, butler, v1, v2, 
-                       xxx_remove_ccds=[1],
-                       xxx_rename_n=5,
-                       xxx_check_for_override_border=10,
-                       xxx_pexConfig_plot=False,
-                       zmax=.04, fig=None, display=False, GAIN=None, sigma=5):
+def xcorrFromVisitPair(self, dataRef, v1, v2):
     """Return the cross-correlation from a given pair of visits.
 
     This is code preforms some preliminary operations and then calls the main correlation calc code.
@@ -24,37 +19,19 @@ def xcorrFromVisitPair(self, butler, v1, v2,
         The sigma-clipped-mean flux in the input images
 
     """
-    for i, vs in enumerate([v1, v2, ]):
-        for v in vs:
-            for ccd in ccds:
-                tmp = isr(butler, v, ccd)
-                if ims[i] is None:
-                    ims[i] = tmp
-                    im = ims[i].getMaskedImage()
-                else:
-                    im += tmp.getMaskedImage()
+    ims = [self.isr(dataRef, v1), self.isr(dataRef, v2)]
 
-        nData = len(ccds)*len(vs)
-        if nData > 1:
-            im /= nData
-        means[i] = afwMath.makeStatistics(im, afwMath.MEANCLIP).getValue()
-        # if display:  # TODO: Move to lsstDebug, use proper display
-            # ds9.mtv(trim(ims[i]), frame=i, title=v)
-
-    ims = [self.isr(butler, v1, ccd), self.isr(butler, v2, ccd)]
-    means = [afwMath.makeStatistics(im, afwMath.MEANCLIP).getValue() for im in ims]
-
-    # mean = np.mean(means)
-    xcorrImg, means1 = self._xcorr(*ims, Visits=[v1, v2], n=n, border=border, frame=len(ims)
-                                   if display else None, CCD=[ccds[0]], GAIN=GAIN, sigma=sigma)
+    xcorrImg, xcorrMeans = self._xcorr(im1, im2, gains)
 
     # TODO: Change to lsstDebug
-    if plot:
-        self._plotXcorr(xcorrImg.clone(), (means1[0]+means[1]), title=r"Visits %s; %s, CCDs %s  $\langle{I}\rangle"
+    if False:
+        means = [afwMath.makeStatistics(im, afwMath.MEANCLIP).getValue() for im in ims]
+        self._plotXcorr(xcorrImg.clone(), (xcorrMeans[0]+means[1]),
+                        title=r"Visits %s; %s, CCDs %s  $\langle{I}\rangle"
                         " = %.3f$ (%s) Var = %.4f" %
-                        (self._getNameOfSet(v1), _getNameOfSet(v2), _getNameOfSet(ccds),
-                         (means1[0]+means[1]), ims[0].getFilter().getName(), float(xcorrImg.getArray()[0, 0]) /
-                         (means1[0]+means[1])), zmax=zmax, fig=fig, SAVE=True,
+                        (self._getNameOfSet(v1), self._getNameOfSet(v2), self._getNameOfSet(ccds),
+                         (xcorrMeans[0]+means[1]), ims[0].getFilter().getName(), float(xcorrImg.getArray()[0, 0]) /
+                         (xcorrMeans[0]+means[1])), zmax=zmax, fig=fig, SAVE=True,
                         fileName=(os.path.join(OUTPUT_PATH, ("Xcorr_visit_" + str(v1[0])+"_"+str(v2[0])+"_ccd_" +
                                                              str(ccds[0])+".png"))))
-    return xcorrImg, means1
+    return xcorrImg, xcorrMeans
--- python/lsst/cp/pipe/temp5.py
+++ python/lsst/cp/pipe/temp5.py
@@ -1,22 +1,25 @@
-    def xcorr(self, im1, im2, Visits, n=5, border=20, frame=None, CCD=[1], GAIN=None, sigma=5, biasCorr=0.9241):
-        """Calculate the cross-correlation of two images im1 and im2 (using robust measures of the covariance).
+    def _xcorr(self, im1, im2, gains):
+        """Calculate the cross-correlation of two images im1 and im2 using robust measures of the covariance.
 
-        This is designed to be called through xcorrFromVisit as that performs some simple ISR.
-        Maximum lag is n, and ignore border pixels around the outside. Sigma is the number of sigma passed
-        to sig cut.
+        Maximum lag is maxLag, and ignore border pixels around the outside.
+        Sigma is the number of sigma passed to sig cut.
         GAIN allows user specified GAINS to be used otherwise the default gains are used.
         The biasCorr parameter is used to correct from the bias of our measurements introduced by the sigma cuts.
         This was calculated using the sim. code at the bottom.
         This function returns one quater of the correlation function, the sum of the means of the two images and
         the individual means of the images
         """
+        maxLag = self.config.maxLag
+        border = self.config.nPixBorderXCorr
+        sigma = self.config.nSigmaClipXCorr
+        biasCorr = self.config.nPixBorderXCorr
+
         sctrl = afwMath.StatisticsControl()
         sctrl.setNumSigmaClip(sigma)
-        ims = [im1, im2]
+
         means = [None, None]
         means1 = [None, None]
-        for i, im in enumerate(ims):
-            # ccd = afwCG.cast_Ccd(im.getDetector())
+        for imNum, im in enumerate([im1, im2]):
             ccd = im.getDetector()
             try:
                 frameId = int(re.sub(r"^SUPA0*", "", im.getMetadata().get("FRAMEID")))
@@ -33,39 +36,35 @@
             except AttributeError:
                 pass
             # im = trim(im, ccd)
-            means[i] = afwMath.makeStatistics(im[border:-border, border:-border],
-                                              afwMath.MEANCLIP, sctrl).getValue()
+            means[imNum] = afwMath.makeStatistics(im[border:-border, border:-border],
+                                                  afwMath.MEANCLIP, sctrl).getValue()
             temp = im.clone()
             # Rescale each amp by the appropriate gain and subtract the mean.
-            for j, a in enumerate(ccd):
-                # smi = im[a.getDataSec(True)]
-                # smiTemp = temp[a.getDataSec(True)]
-                smi = im[a.getBBox()]
-                smiTemp = temp[a.getBBox()]
+            for ampNum, amp in enumerate(ccd):
+                # smi = im[amp.getDataSec(True)]
+                # smiTemp = temp[amp.getDataSec(True)]
+                smi = im[amp.getBBox()]
+                smiTemp = temp[amp.getBBox()]
                 mean = afwMath.makeStatistics(smi, afwMath.MEANCLIP, sctrl).getValue()
-                if GAIN == None:
-                    # gain = a.getElectronicParams().getGain()
-                    gain = a.getGain()
-                else:
-                    gain = GAIN[j]
+                gain = gains[ampNum]
                 # gain/=gain
                 smi *= gain
                 print(mean*gain, afwMath.makeStatistics(smi, afwMath.MEANCLIP, sctrl).getValue())
                 smi -= mean*gain
                 smiTemp *= gain
-            means1[i] = afwMath.makeStatistics(
-                temp[border:-border, border:-border], afwMath.MEANCLIP, sctrl).getValue()
+            means1[imNum] = afwMath.makeStatistics(temp[border:-border, border:-border],
+                                                   afwMath.MEANCLIP, sctrl).getValue()
             print(afwMath.makeStatistics(temp[border:-border, border:-border],
                                          afwMath.MEANCLIP, sctrl).getValue())
         #    print(afwMath.makeStatistics(temp, afwMath.MEANCLIP,sctrl).getValue()-
-        #          afwMath.makeStatistics(temp[0:-n,0:-n], afwMath.MEANCLIP,sctrl).getValue())
-        im1, im2 = ims
+        #          afwMath.makeStatistics(temp[0:-maxLag,0:-maxLag], afwMath.MEANCLIP,sctrl).getValue())
+
         #
         # Actually diff the images
         #
-        diff = ims[0].clone()
+        diff = im1.clone()
         diff = diff.getMaskedImage().getImage()
-        diff -= ims[1].getMaskedImage().getImage()
+        diff -= im2.getMaskedImage().getImage()
 
         diff = diff[border:-border, border:-border]
         # diff.writeFits("./Data/Diff_CCD_"+str(CCD)+".fits")
@@ -78,7 +77,7 @@
         bctrl = afwMath.BackgroundControl(nx, ny, sctrl, afwMath.MEANCLIP)
         bkgd = afwMath.makeBackground(diff, bctrl)
         diff -= bkgd.getImageF(afwMath.Interpolate.CUBIC_SPLINE, afwMath.REDUCE_INTERP_ORDER)
-        # diff.writeFits("./Data/Diff_backsub_CCD_"+str(CCD)+".fits")
+
         if frame is not None:
             ds9.mtv(diff, frame=frame, title="diff")
 
@@ -91,24 +90,26 @@
         #
         # Measure the correlations
         #
-        dim0 = diff[0: -n, : -n]
+        dim0 = diff[0: -maxLag, : -maxLag]
         dim0 -= afwMath.makeStatistics(dim0, afwMath.MEANCLIP, sctrl).getValue()
         w, h = dim0.getDimensions()
-        xcorr = afwImage.ImageF(n + 1, n + 1)
-        for di in range(n + 1):
-            for dj in range(n + 1):
-                dim_ij = diff[di:di + w, dj: dj + h].clone()
-                dim_ij -= afwMath.makeStatistics(dim_ij, afwMath.MEANCLIP, sctrl).getValue()
-                dim_ij *= dim0
-                xcorr[di, dj] = afwMath.makeStatistics(dim_ij, afwMath.MEANCLIP, sctrl).getValue()/(biasCorr)
-        L = np.shape(xcorr.getArray())[0]-1
-        XCORR = np.zeros([2*L+1, 2*L+1])
-        for i in range(L+1):
-            for j in range(L+1):
-                XCORR[i+L, j+L] = xcorr.getArray()[i, j]
-                XCORR[-i+L, j+L] = xcorr.getArray()[i, j]
-                XCORR[i+L, -j+L] = xcorr.getArray()[i, j]
-                XCORR[-i+L, -j+L] = xcorr.getArray()[i, j]
-        print(sum(means1), xcorr.getArray()[0, 0], np.sum(XCORR), xcorr.getArray()[0, 0]/sum(means1),
-              np.sum(XCORR)/sum(means1))
+        xcorr = afwImage.ImageF(maxLag + 1, maxLag + 1)
+        for xlag in range(maxLag + 1):
+            for ylag in range(maxLag + 1):
+                dim_xy = diff[xlag:xlag + w, ylag: ylag + h].clone()
+                dim_xy -= afwMath.makeStatistics(dim_xy, afwMath.MEANCLIP, sctrl).getValue()
+                dim_xy *= dim0
+                xcorr[xlag, ylag] = afwMath.makeStatistics(dim_xy, afwMath.MEANCLIP, sctrl).getValue()/(biasCorr)
+
+        # L = np.shape(xcorr.getArray())[0]-1
+        # XCORR = np.zeros([2*L+1, 2*L+1])
+        xcorr_full = self._tileArray(xcorr)
+        # for i in range(L+1):
+        #     for j in range(L+1):
+        #         XCORR[i+L, j+L] = xcorr.getArray()[i, j]
+        #         XCORR[-i+L, j+L] = xcorr.getArray()[i, j]
+        #         XCORR[i+L, -j+L] = xcorr.getArray()[i, j]
+        #         XCORR[-i+L, -j+L] = xcorr.getArray()[i, j]
+        print(sum(means1), xcorr.getArray()[0, 0], np.sum(xcorr_full), xcorr.getArray()[0, 0]/sum(means1),
+              np.sum(xcorr_full)/sum(means1))
         return (xcorr, means1)
