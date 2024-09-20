9ea44ed609ef58059a58509155b28dda619ccdff
Eli Rykoff <erykoff@stanford.edu>
2024-09-20 12:00:14 -0700

    Add configureIsrTaskLSSTForCalibrations function.

--- python/lsst/cp/pipe/__init__.py
+++ python/lsst/cp/pipe/__init__.py
@@ -20,6 +20,7 @@
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #
 
+from ._configure import *
 from .cpBfk import *
 from .cpCombine import *
 from .cpCrosstalk import *
