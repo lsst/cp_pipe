Fix scaling mask bug.

python/lsst/cp/pipe/cpLinearitySolve.py
@@ -614,11 +614,11 @@ class LinearitySolveTask(pipeBase.PipelineTask):
                 nodes = np.linspace(0.0, np.max(inputOrdinate[mask]), self.config.splineKnots)
 
                 if temperatureValues is not None:
-                    temperatureValuesScaled = temperatureValues - np.median(temperatureValues[~mask])
+                    temperatureValuesScaled = temperatureValues - np.median(temperatureValues[mask])
                 else:
                     temperatureValuesScaled = None
                 if mjds is not None:
-                    mjdsScaled = mjds - np.median(mjds[~mask])
+                    mjdsScaled = mjds - np.median(mjds[mask])
                 else:
                     mjdsScaled = None
 